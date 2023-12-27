5751d84f071f473bd277fbb43f0b1570c557ffb2
Rafael Marcos <rafix995@gmail.com>
2023-12-28 00:28:01 +0100

    Examples menu

---
+++ app/src/main/java/ao/rafaelmarcos/avaliaointents/MainActivity.java
@@ -0,0 +1,23 @@
+package ao.rafaelmarcos.avaliaointents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity{
+    private Button button1, button2, button3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.botao_1);
+        button2 = findViewById(R.id.botao_2);
+        button3 = findViewById(R.id.botao_3);
+    }
+}
